6e1331275e541f37ca4ffe18aa3aef4806aa9862
plurip-lambda <plurip.software@gmail.com>
2023-06-01 11:25:26 +0200

    refactored the Image type + adjusted helper fs

--- src/Types.hs
+++ src/Types.hs
@@ -2,11 +2,12 @@
 
 module Types (Quest(..), Logo(..), Meta(..), Label(..), Image (..), Pixel8, logoFromList) where
 
-import           Codec.Picture        (Image (..), Pixel8,
-                                       PixelBaseComponent (..), readImage)
+import           Codec.Picture        (Pixel8, PixelBaseComponent (..),
+                                       readImage)
+import qualified Codec.Picture        as CP
+import Data.Vector.Storable ( Vector(..), fromList )
 import           Data.List            (intercalate)
-import           Data.Vector.Storable (fromList)
-import           Path                 (Path, path)
+import           Path                 (Path (..), path)
 
 data Quest
     = Quest Logo Meta
@@ -14,29 +15,43 @@ data Quest
 instance Show Quest where
     show (Quest logo meta) = showWithLabel (Label "Quest") [show logo, show meta]
 
-newtype Logo = Logo Image'
+newtype Logo = Logo Image
 
-logoFromList :: [Pixel8] -> Logo
-logoFromList pixels =
-    Logo $
-        Image
-            { imageWidth  = 1
-            , imageHeight = length pixels
-            , imageData   = fromList (pixels :: [Pixel8])
+logoFromList :: Dimensions -> [Pixel8] -> Logo
+logoFromList dimensions' =
+    Logo . createImage dimensions' . Data . fromList
+
+createImage :: Dimensions -> Data -> Image
+createImage (Dimensions {width = width', height = height'}) (Data data')  =
+    ImageBasic $
+        CP.Image
+            { CP.imageWidth  = width'
+            , CP.imageHeight = height'
+            , CP.imageData   = data'
             }
+
+data Dimensions =
+    Dimensions 
+        { width  :: Int
+        , height :: Int
+        }
+
+newtype Data = Data (Vector (PixelBaseComponent Pixel8))
 
 instance Show Logo where
     show (Logo image') = showWithLabel (Label "Logo") [show image']
 
-newtype Meta = Meta [Image']
+newtype Meta = Meta [Image]
 
 instance Show Meta where
     show (Meta images) =
         showWithLabel (Label "Meta") [show images]
 
-type Image' = Image Pixel8
+data Image
+    = ImageFull Path (CP.Image Pixel8)
+    | ImageBasic (CP.Image Pixel8)
 
-instance Show Image' where
+instance Show Image where
   show image' =
     showWithLabel (Label "Image") [show image']
 
