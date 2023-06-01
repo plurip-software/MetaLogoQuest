382a219e8a3eb5f56e7973b9ac5485c81d766328
plurip-lambda <plurip.software@gmail.com>
2023-06-01 09:01:59 +0200

    refactored types + added helper fs

--- src/Types.hs
+++ src/Types.hs
@@ -1,36 +1,50 @@
 {-# LANGUAGE FlexibleInstances #-}
 
-module Types (Quest(..), Logo(..), Image(..), Meta(..)) where
+module Types (Quest(..), Logo(..), Meta(..), Label(..), Image (..), Pixel8, logoFromList) where
 
 import           Codec.Picture        (Image (..), Pixel8,
                                        PixelBaseComponent (..), readImage)
 import           Data.List            (intercalate)
-import qualified Data.Vector.Storable as V
+import           Data.Vector.Storable (fromList)
 import           Path                 (Path, path)
 
 data Quest
     = Quest Logo Meta
 
 instance Show Quest where
-    show (Quest logo meta) = "Quest " <> show logo <> " " <> show meta
+    show (Quest logo meta) = showWithLabel (Label "Quest") [show logo, show meta]
 
 newtype Logo = Logo Image'
 
+logoFromList :: [Pixel8] -> Logo
+logoFromList pixels =
+    Logo $
+        Image
+            { imageWidth  = 1
+            , imageHeight = length pixels
+            , imageData   = fromList (pixels :: [Pixel8])
+            }
+
 instance Show Logo where
-    show (Logo (Image _ _ image')) =
-        "Logo : " <>
-        show image'
+    show (Logo image') = showWithLabel (Label "Logo") [show image']
 
 newtype Meta = Meta [Image']
 
 instance Show Meta where
     show (Meta images) =
-        "Meta : " <>
-        intercalate "," (map show images)
+        showWithLabel (Label "Meta") [show images]
 
 type Image' = Image Pixel8
 
 instance Show Image' where
-  show (Image _ _ image') =
-    "Image : " <>
-    V.foldl (\acc pixel -> acc <> show pixel) "" image'
+  show image' =
+    showWithLabel (Label "Image") [show image']
+
+newtype Label = Label String
+
+instance Show Label where
+    show (Label label') = label' <> " => "
+
+showWithLabel :: Label -> [String] -> String
+showWithLabel label' types' =
+    foldl (<>) "" $ show label' : types'
