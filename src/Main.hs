db7e8e60d3077f1827818a3eef6dc2d8d4d3c858
plurip-lambda <plurip.software@gmail.com>
2023-06-02 04:42:42 +0200

    created readin img logic

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,11 @@
 module Main(main) where
 
-import Types
+import Data ()
+import Codec.Picture ( readImage )
+import Path (path, Extention (PNG))
+import Codec.Picture.Types ( DynamicImage )
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main :: IO (Either String DynamicImage)
+main = do
+    img  <- readImage . show $ path ["imgs"] "logo" PNG
+    return img
